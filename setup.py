d086679caccbfafc09195c51fd8d5e905ba70b8c
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-05-26 11:05:36 +0200

    improve description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.1',
     url='https://github.com/django-beam/django-beam',
     download_url='https://github.com/django-beam/django-beam/archive/0.0.1.tar.gz',
-    description='A workflow library for python',
+    description='A crud library for python',
     packages=find_packages(),
     author='Raphael Kimmig',
     author_email='raphael@ampad.de',
