fb23ccde1f6d69b9504931859e28ea96203a994f
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-05-26 11:02:49 +0200

    add author

--- setup.py
+++ setup.py
@@ -7,5 +7,7 @@ setup(
     download_url='https://github.com/django-beam/django-beam/archive/0.0.1.tar.gz',
     description='A workflow library for python',
     packages=find_packages(),
+    author='Raphael Kimmig',
+    author_email='raphael@ampad.de',
     install_requires=['django >= 1.11', ],
 )
