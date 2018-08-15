07674823ebca787a0b551e218f7f3cf9d587c50d
Raphael Kimmig <raphael.kimmig@ampad.de>
2018-08-15 11:42:53 +0200

    add basic navigation

--- src/beam/templatetags/beam_tags.py
+++ src/beam/templatetags/beam_tags.py
@@ -1,6 +1,10 @@
 from django import template
+from django.apps import apps
 from django.db.models.fields.files import ImageFieldFile, FieldFile
+from django.template.loader import get_template
 from django.urls import NoReverseMatch
+
+from beam.registry import default_registry
 
 register = template.Library()
 
@@ -30,3 +34,18 @@ def is_image(model_field):
 @register.filter
 def is_file(model_field):
     return isinstance(model_field, FieldFile)
+
+
+@register.simple_tag(takes_context=True)
+def render_navigation(context):
+    grouped = []
+    for app_label, viewsets_dict in default_registry.items():
+        group = {
+            "app_label": app_label,
+            "app_config": apps.get_app_config(app_label),
+            "viewsets": viewsets_dict.values(),
+        }
+        grouped.append(group)
+    navigation_template = get_template("beam/navigation.html")
+    request = context.get("request", None)
+    return navigation_template.render({"apps": grouped, "request": request})
